873d81684646a9d4f20cac64d5ff513baaa5fed9
Graeme Brown <graemehbrown@googlemail.com>
2018-02-26 21:50:33 +0000

    Added create, update and delete routes to films controller.

--- controllers/films.js
+++ controllers/films.js
@@ -17,6 +17,29 @@ filmRouter.get('/', function (req, res) {
     res.json({data: filmArray});
 });
 
+// create new film
+filmRouter.post('/', function (req, res) {
+    const film = new Film({
+        title: req.body.title,
+        actors: req.body.actors
+    });
+    filmArray.push(film);
+    res.json({data: filmArray});
+});
 
+//update
+filmRouter.put('/:id', function (req, res) {
+    const film = filmArray[req.params.id]
+    film.title = req.body.title;
+    film.actors = req.body.actors;
+    film.reviews = req.body.reviews;
+    res.json({data: filmArray});
+});
+
+//delete
+filmRouter.delete('/:id', function (req, res) {
+    filmArray.splice(req.params.id, 1);
+    res.json({data: filmArray});
+});
 
 module.exports = filmRouter;
