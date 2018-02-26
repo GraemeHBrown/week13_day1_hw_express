5f8dd622d4e6d313a7d2807d012ddef9d60cec26
Graeme Brown <graemehbrown@googlemail.com>
2018-02-26 18:02:12 +0000

    First commit of express hw.

---
+++ controllers/films.js
@@ -0,0 +1,11 @@
+//since we don't have a database we'll use our front end models at the moment
+const films = require('../client/src/models/films');
+const Film = require('../client/src/models/film');
+const Review = require('../client/src/models/review');
+
+const express = require('express');
+const filmRouter = new express.Router();
+
+
+
+module.exports = filmRouter;
